f20e9e9bfa1a36484f0f9484f9a4097c7ce8391c
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-09-15 13:21:16 -0300

    Using require to load the external template

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,21 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: `<div>
-        <h1>{{header}}</h1>
-        <ul>
-            <li *ngFor="let color of colors">
-                {{color}}
-            </li>
-        </ul>
-        <form>
-            <div>
-                <label for="new-color-input">New Color:</label>
-                <input type="text" id="new-color-input" name="newColor" [(ngModel)]="newColor">
-            </div>
-            <button type="button" (click)="addColor()">Add Color</button>
-        </form>
-    </div>`,
+    template: require("./app.component.html"),
 })
 export class AppComponent {
 
