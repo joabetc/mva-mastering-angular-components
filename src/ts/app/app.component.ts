59f0a7238a551af93291e57e70fc8f2adb64d5ea
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-09-15 13:03:54 -0300

    Displaying a List of Colors

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -4,10 +4,18 @@ import { Component } from "@angular/core";
     selector: "main",
     template: `<div>
         <h1>{{header}}</h1>
+        <ul>
+            <li *ngFor="let color of colors">
+                {{color}}
+            </li>
+        </ul>
     </div>`,
 })
 export class AppComponent {
 
     public header: string = "Color Tool";
 
+    public colors: string[] = [
+        "red", "white", "green", "orange", "blue",
+    ];
 }
