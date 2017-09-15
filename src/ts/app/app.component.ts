1bc7439f5fea7c030457680b5a97682ea7f3c7a6
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-09-15 12:55:37 -0300

    Fixing header expression

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "main",
     template: `<div>
-        <h1>{{message}}</h1>
+        <h1>{{header}}</h1>
     </div>`,
 })
 export class AppComponent {
